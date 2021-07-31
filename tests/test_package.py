445aabecee02c62a8bebab927777b9a797ce34c6
clarkmonkey <clarkmonkey@163.com>
2021-07-31 22:14:53 +0800

    [ADD] Add simple package case.

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,14 +3,15 @@
 # DATE: 2021/7/28
 # Author: clarkmonkey@163.com
 
+import pytest
 from typing import *
 
 
 def test_package() -> NoReturn:
-    """
-    
-    Args: 
 
-    Returns:
+    import cache3
+    assert cache3.__doc__ == "A safe and light Python cache library."
 
-    """
+
+if __name__ == '__main__':
+    pytest.main()
