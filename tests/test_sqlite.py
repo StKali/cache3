1822f6ad4522da9927a1841b0a116034adf92d0b
clarkmonkey <clarkmonkey@163.com>
2021-11-20 15:29:46 +0800

    [ADD] multi-threads test .

---
+++ tests/test_sqlite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# DATE: 2021/11/13
+# Author: clarkmonkey@163.com
+
+from typing import *
+
+
+def test_sqlite() -> NoReturn:
+    """
+    
+    Args: 
+
+    Returns:
+
+    """
